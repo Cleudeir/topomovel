bc06e27ec5ba0365e0b935b97587da497fa4afa2
cleudeir <cleudeirsilva@gmail.com>
2022-05-30 02:29:54 -0300

    correcao da imprecissao

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -3,71 +3,97 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 import { Magnetometer } from 'expo-sensors';
 
-const a1 = []
-
-const a2 = []
+const data = []
 export default function TabThreeScreen() {
 
-  const [useNorte,setNorte] = useState(0)
-  const [useNorteAccuracy,setNorteAccuracy] = useState(0)
+  const [useN,setN] = useState(0)
 
-  const [useInclinacao,setInclinacao] = useState(0)
-  const [useIncliAccuracy,setIncliAccuracy] = useState(0)
+  const [useI,setI] = useState(0)
 
-  const [UpdateInterval] = useState(500)
-  
+
+  const [UpdateInterval] = useState(100)
+
   function _Accelerometer(){
     Accelerometer.setUpdateInterval(UpdateInterval)
+    let iRadiano = 0
     Accelerometer.addListener(e => {
-      const {x,y,z} = e
-      const i = Math.atan2(y,z) * (180/Math.PI)
-     
-      if(a1.length < 1000){
-        a1.push(i)
-        console.log(a1.length)
-      }
-      if(a1.length === 1000){
-        setIncliAccuracy(a1.reduce((a,b)=>a += b,0)/a1.length)
-      }     
-      setInclinacao(i);
+      const {x,y,z} = e  
+      iRadiano =  Math.atan2(y,z) 
+      const iGraus = iRadiano * (180/Math.PI)
+      setI(iRadiano);
     })
+
   }
-  function _Magnetometer(){
+  
+  async function _Magnetometer(){
     Magnetometer.setUpdateInterval(UpdateInterval)
+    let nRadiano = 0
+    
     Magnetometer.addListener(e => {
       const {x,y,z} = e
-      const n = 90 - Math.atan2(y,x) * (180/Math.PI) 
-      if(a2.length < 1000){
-        a2.push(n)
-        console.log(a2.length)
-      }
-      if(a2.length === 1000){
-        setNorteAccuracy(a2.reduce((a,b)=>a += b,0)/a2.length)
-      }  
-      setNorte(n);
-    })
+    
+      nRadiano = 90 * Math.PI/180 - Math.atan2(y,x)
+      setN(nRadiano);
+    })    
   }
 
+  function _start({useN,useI}){
+
+    const atual : { x: number, y: number, z: number , a: number , w: number} = {
+      x : 0,
+      y : 0,
+      z : 0,
+      a : 0,
+      w : 0
+    }
+    const distancia = 1000
+    // Xi= X n-1 ± [ Sen(ω(n-1)-ωn) * Cos(α) ]* D
+    // Yi=Y n-1 +[ Cos(ω(n-1)-ωn) * Cos(α) ]* D
+    // Zi= Z n-1 + D * Sen(α)
+    let anterior = 0
+    if(data[data.length-1]){
+       anterior = data[data.length-1]
+    }else{
+      anterior = atual
+    }
+    
+    atual.a = useI
+    atual.w = useN
+
+    const alteracaoX = (Math.sin(anterior.w - atual.w)  *  Math.cos(atual.a) ) * distancia 
+    const alteracaoY = ( Math.cos(anterior.w - atual.w)  *  Math.cos(atual.a) ) * distancia
+    const alteracaoZ = ( Math.sin(atual.a)) * distancia
+
+    if(
+      alteracaoX> 50 || alteracaoX < -50 
+      && alteracaoZ> 10 || alteracaoZ < -10 
+      && distancia - alteracaoY> 1 || distancia - alteracaoY < -1 
+      )
+    {
+      console.log('x: ',alteracaoX,'y: ',distancia-alteracaoY,'z: ', alteracaoZ)
+      
+      atual.x = Math.round(  anterior.x + alteracaoX )
+      atual.y = Math.round( anterior.y +  alteracaoY)
+      atual.z = Math.round( anterior.z + alteracaoZ )
+      data.push(atual)
+      console.log(atual)
+    }
+  }
   useEffect(() => {
     _Accelerometer()
     _Magnetometer()
-  });
+    _start({useN,useI})
+  }),[useN,useI];
   
   const freq = 500
   return (
     <View style={styles.container}>
       <Text>
-      Norte: {useNorte}  
+      Norte: {useN * (180/Math.PI)}  
       </Text>
+
       <Text>
-      Accuracy Norte: {useNorteAccuracy}
-      </Text>
-     
-      <Text>
-      Inclinação: {useInclinacao}
-      </Text>
-      <Text>
-      Accuracy Inclinação: {useIncliAccuracy}
+      Inclinação: {useI * (180/Math.PI)}
       </Text>
     </View>
   );
